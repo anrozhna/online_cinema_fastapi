cec6579b6748e3cb059207644b397b1bb089a358
Anna <annalepiska@gmail.com>
2026-09-09 18:29:26 +0300

    feat: add ActivationToken, PasswordResetToken, RefreshToken models

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,19 +1,23 @@
 import enum
-from datetime import datetime, date
+from datetime import date, datetime, timedelta, timezone
 from typing import Optional
 
 from sqlalchemy import (
     Boolean,
+    Date,
     DateTime,
     Enum,
     ForeignKey,
     Integer,
     String,
-    func, Date, Text,
+    Text,
+    UniqueConstraint,
+    func,
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.models.base import Base
+from security.utils import generate_secure_token
 
 
 class UserGroupEnum(str, enum.Enum):
@@ -85,3 +89,51 @@ class UserProfile(Base):
         ForeignKey("users.id", ondelete="CASCADE"), nullable=False, unique=True
     )
     user: Mapped[User] = relationship("User", back_populates="profile")
+
+
+class BaseToken(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    token: Mapped[str] = mapped_column(
+        String(64), nullable=False, unique=True, default=generate_secure_token
+    )
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=1),
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
+
+class ActivationToken(BaseToken):
+    __tablename__ = "activation_tokens"
+
+    user: Mapped[User] = relationship("User", back_populates="activation_token")
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+    def __repr__(self):
+        return (
+            f"<ActivationToken(id={self.id}, token={self.token}, "
+            f"expires_at={self.expires_at})>"
+        )
+
+
+class PasswordResetToken(BaseToken):
+    __tablename__ = "password_reset_tokens"
+
+    user: Mapped[User] = relationship("User", back_populates="password_reset_token")
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+
+class RefreshToken(BaseToken):
+    __tablename__ = "refresh_tokens"
+
+    user: Mapped[User] = relationship("User", back_populates="refresh_tokens")
+    token: Mapped[str] = mapped_column(
+        String(512), unique=True, nullable=False, default=generate_secure_token
+    )
