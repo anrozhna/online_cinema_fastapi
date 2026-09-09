3226d928a3eb3cad014a813a7fbdf3a77d0ff1ed
Anna <annalepiska@gmail.com>
2026-09-09 18:15:19 +0300

    feat: add UserGroup model

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,5 +1,10 @@
 import enum
 
+from sqlalchemy import Integer, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.base import Base
+
 
 class UserGroupEnum(str, enum.Enum):
     USER = "user"
@@ -10,3 +15,17 @@ class UserGroupEnum(str, enum.Enum):
 class GenderEnum(str, enum.Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class UserGroup(Base):
+    __tablename__ = "user_groups"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[UserGroupEnum] = mapped_column(
+        Enum(UserGroupEnum), nullable=False, unique=True
+    )
+
+    users: Mapped[list["User"]] = relationship("User", back_populates="group")
+
+    def __repr__(self):
+        return f"<UserGroup(id={self.id}, name={self.name})>"
