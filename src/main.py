c1f42c4b7d330e2130c18f8da68cd2c95feff986
Anna <annalepiska@gmail.com>
2026-09-15 18:59:38 +0300

    feat: update main.py with profile router inclusion

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from routes import accounts_router
+from routes import accounts_router, profiles_router
 
 app = FastAPI()
 
 app.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
 
 
 @app.get("/")
