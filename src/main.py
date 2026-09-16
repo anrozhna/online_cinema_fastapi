f7e1c65c82864e113e78c37fe3a318e54a056cae
Anna <annalepiska@gmail.com>
2026-09-16 15:12:58 +0300

    fix: delete unnecessary code

--- src/main.py
+++ src/main.py
@@ -7,13 +7,3 @@ app = FastAPI()
 app.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
 app.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
 app.include_router(admin_router, prefix="/admin", tags=["admin"])
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
