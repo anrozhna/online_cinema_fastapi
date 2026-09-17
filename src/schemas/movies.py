5a4212c593956c91a1e41e79361e02941a50922c
Anna <annalepiska@gmail.com>
2026-09-17 13:02:13 +0300

    feat: add movie list and detail schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,3 +1,5 @@
+import uuid as uuid_lib
+
 from pydantic import BaseModel
 
 
@@ -37,5 +39,33 @@ class CertificationBaseSchema(BaseModel):
 
 class CertificationResponseSchema(CertificationBaseSchema):
     id: int
+
+    model_config = {"from_attributes": True}
+
+
+class MovieBaseSchema(BaseModel):
+    uuid: uuid_lib.UUID
+    name: str
+    year: int
+    time: int
+    imdb: float
+
+
+class MovieDetailSchema(MovieBaseSchema):
+    votes: int
+    meta_score: float | None = None
+    gross: float | None = None
+    description: str
+    price: float
+    certification: CertificationResponseSchema
+    genres: list[GenreResponseSchema]
+    directors: list[DirectorResponseSchema]
+    stars: list[StarResponseSchema]
+
+    model_config = {"from_attributes": True}
+
+
+class MovieListSchema(MovieBaseSchema):
+    genres: list[GenreBaseSchema]
 
     model_config = {"from_attributes": True}
