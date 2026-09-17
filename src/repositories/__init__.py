aba16a3e6954385855db6dfb35f8b5560afd4a7d
Anna <annalepiska@gmail.com>
2026-09-17 15:25:51 +0300

    feat: add new to __init__.py

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,4 +1,5 @@
 from repositories.base import BaseRepository
+from repositories.comments import CommentRepository
 from repositories.movies import (
     CertificationRepository,
     DirectorRepository,
@@ -7,6 +8,8 @@ from repositories.movies import (
     StarRepository,
 )
 from repositories.profiles import ProfileRepository
+from repositories.ratings import RatingRepository
+from repositories.reactions import MovieReactionRepository
 from repositories.tokens import (
     ActivationTokenRepository,
     PasswordResetTokenRepository,
@@ -27,4 +30,7 @@ __all__ = [
     "GenreRepository",
     "MovieRepository",
     "StarRepository",
+    "CommentRepository",
+    "MovieReactionRepository",
+    "RatingRepository",
 ]
